85d4792e4aa05a6ffc1a5193b85b8f9a00ba0de1
abdul mukti <abdulmukti40390@gmail.com>
2025-07-20 11:58:20 +0700

    change sql

--- inventarisapp.sql
+++ inventarisapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 14, 2025 at 01:05 AM
+-- Generation Time: Jul 20, 2025 at 04:57 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.16
 
@@ -58,6 +58,27 @@ CREATE TABLE `cache_locks` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, 'Alat Tulis update', 'Termasuk Map Pulpen, Buku dll', '2025-07-19 11:31:28', '2025-07-19 11:40:47');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `distributions`
 --
 
@@ -71,14 +92,6 @@ CREATE TABLE `distributions` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `distributions`
---
-
-INSERT INTO `distributions` (`id`, `tanggal_pengeluaraan`, `item_id`, `jumlah`, `unit_penerima`, `petugas_penyerah`, `created_at`, `updated_at`) VALUES
-(6, '2025-06-16', 78, '5', 'Ruang TIKIM', 'Haryati Apri Yani', '2025-06-17 04:26:49', '2025-06-17 04:26:49'),
-(7, '2025-06-11', 82, '5', 'Ruang TIKIM', 'Haryati Apri Yani', '2025-06-17 04:40:13', '2025-06-17 04:40:13');
 
 -- --------------------------------------------------------
 
@@ -119,7 +132,7 @@ CREATE TABLE `financial_reports` (
 --
 
 INSERT INTO `financial_reports` (`id`, `periode`, `total_anggaran`, `total_realisasi`, `selisih`, `created_at`, `updated_at`, `rincian_transaksi`, `bulan_periode`) VALUES
-(9, 'bulanan', '500000000', '0', '500000000', '2025-07-14 00:19:47', '2025-07-14 00:57:08', 'Tidak Ada', '2025-07');
+(14, 'tahunan', '100000000', '1000000', '99000000', '2025-07-20 03:42:05', '2025-07-20 03:42:18', 'Tidak Ada', '2025-07');
 
 -- --------------------------------------------------------
 
@@ -137,21 +150,16 @@ CREATE TABLE `items` (
   `stok` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lokasi_penyimpanan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `category_id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `kode_barang`, `nama_barang`, `spesifikasi`, `satuan`, `kategori`, `stok`, `lokasi_penyimpanan`, `created_at`, `updated_at`) VALUES
-(73, 'BRG-7253', 'MAP Merah', 'Bahan: kertas karton, Warna: Merah', 'Buah', 'ATK', '60', 'Gudang ATK', '2025-06-17 03:17:20', '2025-06-17 03:17:20'),
-(74, 'BRG-7254', 'MAP Kuning', 'Bahan: kertas karton, Warna: Kuning', 'Buah', 'ATK', '60', 'Gudang ATK', '2025-06-17 03:18:43', '2025-07-14 00:57:08'),
-(78, 'BRG-7200', 'Bolpoin', 'warna: hitam, merk: Standar', 'Kotak', 'ATK', '0', 'Gudang ATK', '2025-06-17 04:24:47', '2025-07-13 23:36:18'),
-(82, 'BRG-7222', 'Tinta Stempel', 'warna: hitam', 'Botol', 'ATK', '5', 'Gudang ATK', '2025-06-17 04:38:34', '2025-07-13 04:34:25'),
-(83, 'BRG-7525', 'Kertas HVS A4', 'warna: putih, ukuran : A4, merk:sidu', 'Ream', 'ATK', '20', 'Gudang ATK', '2025-06-17 04:42:18', '2025-06-17 04:42:18'),
-(84, 'BRG-1978', 'Laptop Lenovo ThinkPad E14', 'Intel i5, RAM 8GB, SSD 512GB, 14 inch', 'Buah', 'Laptop', '-5', 'Ruang Keuangan', '2025-06-17 04:43:14', '2025-07-14 00:51:11'),
-(86, 'BRG-1977', 'kajajajss', 'jajajaj', 'jajaja', 'jajajaja', '-800', 'jajaja', '2025-06-23 20:57:21', '2025-07-13 04:34:29');
+INSERT INTO `items` (`id`, `kode_barang`, `nama_barang`, `spesifikasi`, `satuan`, `kategori`, `stok`, `lokasi_penyimpanan`, `created_at`, `updated_at`, `category_id`) VALUES
+(88, 'BRG0001', 'Buku', 'Buku', '100', '0', '22', 'gudang', '2025-07-20 03:29:46', '2025-07-20 03:42:18', 1);
 
 -- --------------------------------------------------------
 
@@ -223,7 +231,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (16, '2025_07_13_101059_create_pagu_anggarans_table', 14),
 (17, '2025_07_13_103002_rename_tahun_anngaran_column_in_pagu_anggarans_table', 15),
 (18, '2025_07_13_111957_add_column_to_financial_reports_table', 16),
-(19, '2025_07_13_112301_add_bulan_priode_to_financial_reports_table', 17);
+(19, '2025_07_13_112301_add_bulan_priode_to_financial_reports_table', 17),
+(20, '2025_07_19_181906_create_categories_table', 18),
+(21, '2025_07_19_184245_add_category_id_to_items_table', 19),
+(22, '2025_07_19_193149_add_category_id_to_pagu_anggarans_table', 20);
 
 -- --------------------------------------------------------
 
@@ -238,15 +249,16 @@ CREATE TABLE `pagu_anggarans` (
   `bulan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nilai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `category_id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `pagu_anggarans`
 --
 
-INSERT INTO `pagu_anggarans` (`id`, `priode`, `tahun_anggaran`, `bulan`, `nilai`, `created_at`, `updated_at`) VALUES
-(7, NULL, '2025', '2025-07', '500000000', '2025-07-14 00:19:47', '2025-07-14 00:19:47');
+INSERT INTO `pagu_anggarans` (`id`, `priode`, `tahun_anggaran`, `bulan`, `nilai`, `created_at`, `updated_at`, `category_id`) VALUES
+(11, NULL, '2025', '2025-07', '100000000', '2025-07-20 03:42:05', '2025-07-20 03:42:05', 1);
 
 -- --------------------------------------------------------
 
@@ -279,6 +291,13 @@ CREATE TABLE `procruments` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `procruments`
+--
+
+INSERT INTO `procruments` (`id`, `nomor_pengadaan`, `tanggal_pengadaan`, `suplier_name`, `item_id`, `jumlah_barang`, `total_biaya`, `status`, `created_at`, `updated_at`) VALUES
+(41, '910191921', '2025-07-20', 'Bahlil', 88, '16', 1000000, 'diterima', '2025-07-20 03:42:18', '2025-07-20 03:42:18');
+
 -- --------------------------------------------------------
 
 --
@@ -301,11 +320,7 @@ CREATE TABLE `report_items` (
 --
 
 INSERT INTO `report_items` (`id`, `item_id`, `stock_awal`, `jumlah_pemasukkan`, `jumlah_pengeluaran`, `sisa_stok`, `created_at`, `updated_at`) VALUES
-(22, 82, '10', '0', '5', '5', '2025-06-17 04:38:34', '2025-07-13 04:34:25'),
-(23, 83, '20', '0', '0', '20', '2025-06-17 04:42:18', '2025-06-17 04:42:18'),
-(24, 84, '5', '0', '0', '5', '2025-06-17 04:43:14', '2025-07-14 00:51:11'),
-(26, 86, '818', '0', '0', '818', '2025-06-23 20:57:21', '2025-07-13 04:34:29'),
-(28, 78, '100', '0', '5', '95', '2025-07-13 04:51:16', '2025-07-13 23:36:18');
+(30, 88, '6', '16', '0', '22', '2025-07-20 03:29:46', '2025-07-20 03:42:18');
 
 -- --------------------------------------------------------
 
@@ -366,7 +381,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('GcqAvZT8VepirwlFOTG45wyrtw1JWfNVxm4B6blG', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiMmtEN1ZGSHZoYWZrZTV6MGhUUUt0YTM2QWV0VmIzUDA5dnczM1ptciI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wZW55ZWRpYWFuL2NyZWF0ZSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7czo1OiJhbGVydCI7YTowOnt9fQ==', 1752454732);
+('IUIJRca31EV1dvV2r3tfITrBdrS2sqYKwY6p9AmW', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUXJBT0g5djJMUjlVc2VYeUExU0FRRENJb3B2YUFMTjhnZnduOURLbyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sYXBvcmFuIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1752987101);
 
 -- --------------------------------------------------------
 
@@ -389,9 +404,9 @@ CREATE TABLE `settings` (
 INSERT INTO `settings` (`id`, `key`, `value`, `created_at`, `updated_at`) VALUES
 (1, 'site_name', 'InventarisApp', NULL, '2025-06-19 03:13:04'),
 (2, 'email', 'kanimbanjarmasin@gmail.com', '2025-06-16 16:24:34', '2025-06-16 22:08:55'),
-(3, 'address', '081256559196', '2025-06-16 16:47:23', '2025-06-19 03:13:04'),
+(3, 'address', 'Jl.Rajawali Kemuning sari lor 02 , Jember , Jawa Timur , Indonesia', '2025-06-16 16:47:23', '2025-07-20 04:11:21'),
 (4, 'phone', '081256559196', '2025-06-16 16:47:23', '2025-06-16 22:08:55'),
-(5, 'logo', 'setting/3ALWkPNhIRdgnpPo5QxYFINjkDbDJ5HwFn1Vjjs9.png', '2025-06-16 16:50:20', '2025-07-13 16:12:08'),
+(5, 'logo', 'setting/5RuqqmwngaXrrwsvjkETeDcwLDpNGoqwkTfCOr9Q.jpg', '2025-06-16 16:50:20', '2025-07-20 04:10:22'),
 (6, 'favicon', 'setting/r81rquxxBiBm98ejJXu9q9lrNT5iEPrVRhQ6lsHp.png', '2025-06-16 17:54:01', '2025-07-13 16:12:22'),
 (7, 'kepala', 'Muhammad Wahyuni, S.Sos', '2025-06-16 20:49:01', '2025-06-16 20:49:01'),
 (8, 'nip_kepala', '197106141993031002', '2025-06-16 20:49:01', '2025-06-16 20:49:01');
@@ -437,6 +452,12 @@ ALTER TABLE `cache`
 --
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `distributions`
@@ -549,6 +570,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `distributions`
 --
 ALTER TABLE `distributions`
@@ -564,13 +591,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `financial_reports`
 --
 ALTER TABLE `financial_reports`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -582,25 +609,25 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `pagu_anggarans`
 --
 ALTER TABLE `pagu_anggarans`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `procruments`
 --
 ALTER TABLE `procruments`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `report_items`
 --
 ALTER TABLE `report_items`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `requests`
